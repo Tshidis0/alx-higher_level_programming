a1c94f97a113e0108592619bd83237f89c311ab9
Tshidis0 <tshidisorasemetse7@gmail.com>
2024-01-31 01:54:44 -0800

    Updated

--- 0x07-python-test_driven_development/102-tests.py
+++ 0x07-python-test_driven_development/102-tests.py
@@ -8,7 +8,7 @@ s = "ложка не существует"
 lib.print_python_string(s)
 s = "La cuillère n'existe pas"
 lib.print_python_string(s)
-s = "勺孨"
+s = "勺子不存在"
 lib.print_python_string(s)
 s = "숟가락은 존재하지 않는다."
 lib.print_python_string(s)
